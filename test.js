7219d17c5cc4306da8df981845b86a1782a4b571
Punit Narang <mepunit@gmail.com>
2021-08-19 20:04:07 +0530

    config test #3

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const { compileCode } = require('./compile');
 
+console.log('received config: ', JSON.parse(process.env.firmware_config));
+
 compileCode(null, JSON.parse(process.env.firmware_config));
